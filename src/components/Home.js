5b8799d85ccab8f8edd700d629af8cf5c1cafa70
Daniel López <daniel.lopez@viajesfalabella.com>
2020-06-12 20:33:30 -0400

    [imp] cambios en home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,33 @@
 import React from 'react'
-import Container from '@material-ui/core/Container';
 
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+    container: {
+        marginTop: 60
+    },
+    grid: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center'
+    }
+}));
 const Home = props => {
+    const classes = useStyles();
+
     return (
-        <Container>
-            <div>
-                Bienvenido
-            </div>
+        <Container className={classes.container}>
+            <Grid container justify="center" className={classes.grid}>
+                <Typography component="h4" variant="h4" align="center">
+                    Bienvenidos
+                </Typography>
+                <Typography variant="subtitle1" align="center">
+                    Inicia sesión para acceder al contenido.
+                </Typography>
+            </Grid>
         </Container>
     );
 }
